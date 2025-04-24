d1faf8dee388d4cb984032516b70ef8b78e333f6
unknown <20223020057@estudantes.ifpr.edu.br>
2025-04-24 11:11:43 -0300

    Criação de classes e atributos

--- paginas/models.py
+++ paginas/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# todas as classes DEVEM ter herança de models.Model
+# Crie suas classes
+
+
+class Campus(models.Model):
+    # definir atributos
+    nome = models.CharField(max_length=100)
+
+
+class Curso (models.Model):
+    nome = models.CharField(max_length=150)
+    campus = models.ForeignKey(Campus, on_delete=models.PROTECT)
+
+
+class TipoSolicitacao(models.Model):
+    descricao = models.CharField(max_length=250, verbose_name="descrição")
+    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100)
